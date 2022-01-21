925e4fb5cd53b442d037a50de15249164053da26
vibernation <10301251+vibernation@users.noreply.github.com>
2022-01-21 00:27:18 -0600

    added frequencies, numpy, better algorithm, etc

--- wordlehax.py
+++ wordlehax.py
@@ -1,40 +1,49 @@
 from math import log
-
+import numpy as np
 
 # 00000_3 - no words match
 # 11111_3 - all words in wrong position
 # 22222_3 - all words in right position (right)
 
-def comp(word1, word2):
+
+def w_comp(given, correct):
+    values = [(2 if given[i] == correct[i] else None) for i in range(5)]
+    correct_exc = [correct[i] for i in range(5) if values[i] == None]
     final = 0
-    wordsu = word2
     for i in range(5):
         final *= 3
-        if word1[i] == word2[i]:
-            wordsu = wordsu.replace(word1[i], "", 1)
-            final += 2
-        elif (word1[i] in wordsu):
-            wordsu = wordsu.replace(word1[i], "", 1)
-            final += 1
+        if values[i] == None:
+            if given[i] in correct_exc:
+                values[i] = 1
+                correct_exc.remove(given[i])
+            else:
+                values[i] = 0
+        final += values[i]
     return final
 
-def best(wordl):
-    wlen = len(wordl)
-    if wlen == 1:
-        return (wordl[0], 0)
+def best(words, valid):
 
-    arrc = [0] * 3**5
-    max = (None, 0)
+    if len(valid) == 1:
+        return (valid["word"][0], 0)
 
-    for word in wordl:
-        arrc = [0] * 3**5
-        for wordc in wordl:
-            arrc[comp(word, wordc)] += 1/wlen
-        final_s = -sum([x * log(x) for x in arrc if x != 0])
-        if final_s > max[1]:
-            max = (word, final_s)
+    ftot = np.sum(valid["freq"])
+    normalizer = np.vectorize(lambda x: x/ftot, otypes=[float])
+    normalv = normalizer(valid["freq"])
+    info_comp = np.vectorize(lambda x: x * log(x) if x != 0 else 0, otypes=[float])
+    max_info = -np.sum(info_comp(normalv))
+
+
+    max = ("?", 0)
+
+    for word, _ in words:
+        wcf = np.vectorize(lambda w: w_comp(word, w))
+        freq_counter = np.bincount(wcf(valid["word"]), weights=normalv, minlength=3**5)
+        word_info = max_info * freq_counter[-1] - np.sum(info_comp(freq_counter[:-1]))
+        if word_info > max[1]:
+            max = (word, word_info)
 
     return max
+
 
 def conv(strr):
     if strr == "":
@@ -42,7 +51,8 @@ def conv(strr):
     return int(strr[-1]) + conv(strr[:-1]) * 3
 
 def filter(wordl, bes, seq):
-    return [x for x in wordl if comp(bes, x) == seq]
+    w_vec = np.vectorize(lambda w: w_comp(bes, w))
+    return wordl[w_vec(wordl["word"]) == seq]
 
 def checkin(input):
     if len(input) != 5:
@@ -52,34 +62,38 @@ def checkin(input):
             return False
     return True
 
-initwords = []
-file = open("5lw.txt", "r")
-for line in file.readlines():
-    initwords.append(line.strip())
-
 def run():
+    print()
     print("input key:")
     print("0 - gray, 1 - yellow, 2 - green")
     print("e.g: 02100")
     print("================================\n")
 
-    wordlist = initwords
+    initlist = []
+    with open("5lw.txt", "r") as words, open("5lwf.txt", "r") as freqs:
+        for word, freq in zip(words.readlines(), freqs.readlines()):
+            w = str(word.strip())
+            f = float(freq.strip())
+            initlist.append((w, f))
+    dt = [("word", "U5"), ("freq", "float")]
+    initarr = np.array(initlist, dtype=[("word", "U5"), ("freq", "float")])
+    wordlist = initarr
 
     while True:
-        bes = best(wordlist)
-        print("best of " + str(len(wordlist)) +  " word(s) is: " + bes[0] + " (" + str(bes[1]) + " total info)")
-        if len(wordlist) == 1:
-            print("done")
-            return
+        bes = best(initarr, wordlist)
+        print("current best word is " + bes[0] + " (" + str(bes[1]) + " total info)")
 
         inp = input("choose input: ").strip()
         while not checkin(inp):
             if inp == "" or inp.lower() == "exit" or  inp.lower() == "quit" or inp.lower() == "q":
                 return
             inp = input("try input again: ").strip()
+        if(inp == "22222"):
+            print("you win!")
+            return
 
         wordlist = filter(wordlist, bes[0], conv(inp))
-		
+
         if len(wordlist) == 0:
             print("no possible words")
             return
